829a8ff7cf51f4dc7d375b231c19263c582d29b2
mjeng <mjeng@berkeley.edu>
2017-12-18 16:16:12 -0800

    Learned to run things locally so haven't been committing/pushing as much. However encountered problem with photo paths and pushing to heroku to see if that somehow fixes the problem.

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import flask, os
 app = flask.Flask(__name__)
 
 @app.route("/") # decorator specificies what to do when home.page/<decorator URL> is opened
-def hello(): # what runs whenever home page is opened. can put parameters for this function
+def run_main(): # what runs whenever home page is opened. can put parameters for this function
     return flask.render_template("index.html")
 
-@app.route("/test")
-def test():
-    return flask.render_template("test.html")
+@app.route("/photography")
+def run_photos():
+    return flask.render_template("photography.html")
 
 if __name__ == "__main__":
     # app.run()
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='localhost', port=port)
